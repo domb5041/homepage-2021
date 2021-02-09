a30e8115645bba736fcb9e997f91ab6ea3c163d4
dominicbutler <dominic.butler@exonar.com>
2021-02-09 14:07:20 +0000

    testing data driven img

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import projects from './projectData';
+
 function App() {
-    return <div>hello</div>;
+    return (
+        <div>
+            {projects.map((p, i) => (
+                <div>
+                    <img src={'/images/' + p.image} />
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/projectData.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 'p4',
+        url:
+            'https://www.youtube.com/watch?v=JChDbIOkE58&list=PLrRbyFPS5q6w8wLmKAudLnPg1L7CMv1Lw',
+        text: 'lskadjhl askjdfhla skdfjha lskdjfh alksjdfh askljdhf ',
+        image: 'IMG_2091.jpeg',
+    },
+    {
+        id: 'p3',
+        url:
+            'https://www.youtube.com/watch?v=JChDbIOkE58&list=PLrRbyFPS5q6w8wLmKAudLnPg1L7CMv1Lw',
+        text: 'lskadjhl askjdfhla skdfjha lskdjfh alksjdfh askljdhf ',
+        image: 'IMG_2091.jpeg',
+    },
+    {
+        id: 'p1',
+        url:
+            'https://www.youtube.com/watch?v=JChDbIOkE58&list=PLrRbyFPS5q6w8wLmKAudLnPg1L7CMv1Lw',
+        text: 'lskadjhl askjdfhla skdfjha lskdjfh alksjdfh askljdhf ',
+        image: 'IMG_2091.jpeg',
+    },
+    {
+        id: 'p0',
+        url:
+            'https://www.youtube.com/watch?v=JChDbIOkE58&list=PLrRbyFPS5q6w8wLmKAudLnPg1L7CMv1Lw',
+        text: 'lskadjhl askjdfhla skdfjha lskdjfh alksjdfh askljdhf ',
+        image: 'IMG_2091.jpeg',
+    },
+];
